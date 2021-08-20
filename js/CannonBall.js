1c1fc9f927dde3b5db655872511c5c9a28367e25
SAKSHAMTIWARI04 <81856003+SAKSHAMTIWARI04@users.noreply.github.com>
2021-08-20 13:43:21 +0530

    Add files via upload

---
+++ js/CannonBall.js
@@ -0,0 +1,53 @@
+class CannonBall {
+  constructor(x, y) {
+    var options = {
+      restitution: 0.8,
+      friction: 1.0,
+      density: 1.0,
+      isStatic: true
+    };
+    this.r = 40;
+
+    this.body = Bodies.circle(x, y, this.r, options);
+
+    this.image = loadImage("./assets/cannonball.png");
+    this.tower = loadImage("./assets/gray.jpg");
+    this.trajectory=[]
+    World.add(world, this.body);
+  }
+
+  //shooting the cannonball
+  shoot() {
+    var velocity = p5.Vector.fromAngle(cannon.angle);
+    velocity.mult(20);
+    Matter.Body.setStatic(this.body, false);
+    Matter.Body.setVelocity(this.body, { x: velocity.x, y: velocity.y });
+  }
+
+  display() {
+    var angle = this.body.angle;
+    var position1 = this.body.position;
+    if(this.body.velocity.x>0 && this.body.position.x>300) 
+    {
+      var pos=[this.body.position.x,this.body.position.y]
+      this.trajectory.push(pos)
+    }
+    push();
+    rotate(angle);
+    imageMode(CENTER);
+    for(var i=0;i<this.trajectory.length;i++ )
+    {
+      image(this.image, this.trajectory[i][0], this.trajectory[i][1], 5,5);
+    }
+    pop();
+    
+    push();
+    translate(position1.x, position1.y);
+    rotate(angle);
+
+    image(this.image, 0, 0, this.r, this.r);
+    pop();
+
+   
+  }
+}
